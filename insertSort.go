e11ac75e3d1e1a728c7601b9579afd7bdda7ae87
crazyleeyang <crazyleeyang@gmail.com>
2017-11-17 07:54:13 +0800

    Update insertSort.go

--- insertSort.go
+++ insertSort.go
@@ -2,13 +2,17 @@ func insertSort(a []int){
     l := len(a)
     for i:=1;i<l;i++{
         tmp := a[i]
-        for j:=i-1;j>=0;j--{
+        j := i-1
+        for ;j>=0;j--{
             if a[j] > tmp{
                 a[j+1] = a[j]
-                a[j] = tmp
             } else {
                 break
             }
+        }
+
+        if j+1 != i{
+            a[j+1] = tmp
         }
     }
 }
